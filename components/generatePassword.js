faf2132f4afad98447230beb15c60d28845b0382
Begotten-Traveler <solomondev002@gmail.com>
2025-04-05 16:19:32 +0100

    fix: bugs fix

--- components/generatePassword.js
+++ components/generatePassword.js
@@ -1,6 +1,6 @@
 function generatePassword(){
-    const passwordOne = generateRandomPassword();
-    const passwordTwo = generateRandomPassword();
+    const passwordOne = getRandomPassword();
+    const passwordTwo = getRandomPassword();
     firstPasswordBox.textContent = passwordOne;
     secondPasswordBox.textContent = passwordTwo;
 }
